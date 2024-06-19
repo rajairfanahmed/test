a7d91191e71aee9e849bf1f5658069d608c01c3a
Raja Irfan Ahmed <rajairfana551@gmail.com>
2024-06-19 11:49:01 +0500

    first commit

---
+++ dist/index.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementById("hamburger-menu"),t=document.querySelector(".mobile-view");e.addEventListener("click",(()=>{e.classList.toggle("open"),t.classList.toggle("show")}))}))})();
