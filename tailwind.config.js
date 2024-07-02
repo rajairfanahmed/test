af79257a0ffa996e1a54f504e2757b568b23cf92
Raja Irfan Ahmed <rajairfana551@gmail.com>
2024-07-02 12:54:52 +0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,31 @@ module.exports = {
   // ],
  
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xxsm': '320px',
+        'xsm': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+    },
   },
   variants: {
     extend: {},
