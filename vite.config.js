cf7cdfdf906f182558198e786fab5944715ff9b8
Raja Irfan Ahmed <rajairfana551@gmail.com>
2024-07-02 13:30:57 +0500

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import babel from "@rollup/plugin-babel";
 
 export default defineConfig({
+  base: 'test',
   plugins: [
     babel({
       babelHelpers: "runtime",
